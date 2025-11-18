c0d6ede088b61863d83dd14febc8a76c03ef225b
juho-creator <juhokim2018@gmail.com>
2025-11-18 18:44:50 +0900

    [fix] 파일 추가

--- src/domain/ports/repositories/project-repository-interface.ts
+++ src/domain/ports/repositories/project-repository-interface.ts
@@ -1,10 +1,13 @@
-import { PersistProjectEntity, NewProjectEntity, ProjectEntity } from "../../entities/project/project-entity";
+import {
+  PersistProjectEntity,
+  NewProjectEntity,
+  ProjectEntity,
+} from "../../entities/project/project-entity";
 
 export type LockType = "share" | "beta";
 
-export interface IProjectRepository{
-  create(entity: NewProjectEntity) : any;
+export interface IProjectRepository {
+  create(entity: NewProjectEntity): any;
   findById(projectId: number): Promise<PersistProjectEntity | null>;
-  update(entity:ProjectEntity): Promise<PersistProjectEntity>;
+  update(entity: ProjectEntity): Promise<PersistProjectEntity>;
 }
-
