4b4a8f9ce75956144d16d9eecb1bc9c90f913845
juho-creator <juhokim2018@gmail.com>
2025-11-20 16:13:48 +0900

    [Refactor] TaskEntity 구조 개선 및 UserEntity 타입 정리

--- src/2_domain/entites/task/task-entity.ts
+++ src/2_domain/entites/task/task-entity.ts
@@ -1,98 +1,25 @@
 import { User } from "@prisma/client";
-import { TaskReqDto } from "../../../1_inbound/requests/task-req-dto";
+import {
+  CreateTaskReqDto,
+  UpdateTaskReqDto,
+} from "../../../1_inbound/requests/task-req-dto";
 import { TaskResDto } from "../../../1_inbound/responses/task-res-dto";
 import { TagEntity } from "../tag/tag-entity";
 import { UserEntity } from "../user/user-entity";
 import { AttachmentEntity } from "../attachment/attachment-entity";
 import { stat } from "fs";
 
-export class PersistTaskEntity {
-  private readonly _id: number;
-  private readonly _projectId: number;
-  private readonly _title: string;
-  private readonly _startDate: Date | null;
-  private readonly _endDate: Date | null;
-  private readonly _status: string;
-  private readonly _assignee: UserEntity | null;
-  private readonly _tags: TagEntity[];
-  private readonly _attachments: AttachmentEntity[];
-  private readonly _createdAt: Date;
-  private readonly _updatedAt: Date;
-
-  constructor(data: {
-    id: number;
-    projectId: number;
-    title: string;
-    startDate: Date | null;
-    endDate: Date | null;
-    status: string;
-    assignee: UserEntity | null;
-    tags: TagEntity[];
-    attachments: AttachmentEntity[];
-    createdAt: Date;
-    updatedAt: Date;
-  }) {
-    this._id = data.id;
-    this._projectId = data.projectId;
-    this._title = data.title;
-    this._startDate = data.startDate;
-    this._endDate = data.endDate;
-    this._status = data.status;
-    this._assignee = data.assignee;
-    this._tags = data.tags;
-    this._attachments = data.attachments;
-    this._createdAt = data.createdAt;
-    this._updatedAt = data.updatedAt;
-  }
-
-  get id() {
-    return this._id;
-  }
-
-  get projectId() {
-    return this._projectId;
-  }
-
-  get title() {
-    return this._title;
-  }
-
-  get startDate() {
-    return this._startDate;
-  }
-
-  get endDate() {
-    return this._endDate;
-  }
-
-  get status() {
-    return this._status;
-  }
-
-  get assignee() {
-    return this._assignee;
-  }
-
-  get tags() {
-    return this._tags;
-  }
-
-  get attachments() {
-    return this._attachments;
-  }
-
-  get createdAt() {
-    return this._createdAt;
-  }
-
-  get updatedAt() {
-    return this._updatedAt;
-  }
+export interface CreateTaskEntity extends ModifyTaskEntity {
+  projectId: number;
 }
 
-export class TaskEntity {
-  private readonly _taskId: number;
-  private readonly _projectId: number;
+export interface UpdateTaskEntity extends ModifyTaskEntity {
+  taskId: number;
+}
+
+export class ModifyTaskEntity {
+  private readonly _taskId?: number;
+  private readonly _projectId?: number;
   private readonly _title: string;
   private readonly _startDate: Date | null;
   private readonly _endDate: Date | null;
@@ -102,33 +29,72 @@ export class TaskEntity {
   private readonly _tags: string[];
 
   constructor(params: {
-    taskId: number,
-    projectId: number,
-    title: string,
-    startYear: number,
-    startMonth: number,
-    startDay: number,
-    endYear: number,
-    endMonth: number,
-    endDay: number,
-    status: string,
-    attachments: string[],
-    assigneeId: number | null,
-    tags: string[],
+    taskId?: number;
+    projectId?: number;
+    title: string;
+    startYear: number;
+    startMonth: number;
+    startDay: number;
+    endYear: number;
+    endMonth: number;
+    endDay: number;
+    status: string;
+    attachments: string[];
+    assigneeId: number | null;
+    tags: string[];
   }) {
     this._taskId = params.taskId;
     this._projectId = params.projectId;
     this._title = params.title;
 
-    this._startDate = new Date(Date.UTC(params.startYear, params.startMonth - 1, params.startDay));
+    this._startDate = new Date(
+      Date.UTC(params.startYear, params.startMonth - 1, params.startDay),
+    );
 
-    this._endDate = new Date(Date.UTC(params.endYear, params.endMonth - 1, params.endDay));
+    this._endDate = new Date(
+      Date.UTC(params.endYear, params.endMonth - 1, params.endDay),
+    );
 
     this._status = params.status;
     this._attachments = params.attachments;
     this._assigneeId = params.assigneeId;
     this._tags = params.tags;
   }
+
+  static toCreateTaskEntity(dto: CreateTaskReqDto) {
+    return new ModifyTaskEntity({
+      projectId: dto.params.projectId,
+      title: dto.body.title,
+      startYear: dto.body.startYear,
+      startMonth: dto.body.startMonth,
+      startDay: dto.body.startDay,
+      endYear: dto.body.endYear,
+      endMonth: dto.body.endMonth,
+      endDay: dto.body.endDay,
+      status: dto.body.status,
+      attachments: dto.body.attachments,
+      assigneeId: dto.body.assigneeId,
+      tags: dto.body.tags,
+    }) as CreateTaskEntity;
+  }
+
+  static toUpdateTaskEntity(dto: UpdateTaskReqDto) {
+    return new ModifyTaskEntity({
+      taskId: dto.params.taskId,
+      title: dto.body.title,
+      startYear: dto.body.startYear,
+      startMonth: dto.body.startMonth,
+      startDay: dto.body.startDay,
+      endYear: dto.body.endYear,
+      endMonth: dto.body.endMonth,
+      endDay: dto.body.endDay,
+      status: dto.body.status,
+      attachments: dto.body.attachments,
+      assigneeId: dto.body.assigneeId,
+      tags: dto.body.tags,
+    }) as CreateTaskEntity;
+  }
+
   get taskId() {
     return this._taskId;
   }
@@ -163,91 +129,5 @@ export class TaskEntity {
 
   get tags() {
     return this._tags;
-  }
-}
-
-export class ViewProjectTaskEntity {
-  private readonly _projectId: number;
-  private readonly _page?: number;
-  private readonly _limit?: number;
-  private readonly _status?: string;
-  private readonly _assignee?: number;
-  private readonly _keyword?: string;
-  private readonly _order?: string;
-  private readonly _orderBy?: string;
-
-  constructor(
-    projectId: number,
-    page: number | undefined,
-    limit: number | undefined,
-    status: string | undefined,
-    assignee: number | undefined,
-    keyword: string | undefined,
-    order: string | undefined,
-    orderBy: string | undefined
-  ) {
-    this._projectId = projectId;
-    this._page = page;
-    this._limit = limit;
-    this._status = status;
-    this._assignee = assignee;
-    this._keyword = keyword;
-    this._order = order;
-    this._orderBy = orderBy;
-  }
-
-  get projectId() {
-    return this._projectId;
-  }
-
-  get page() {
-    return this._page;
-  }
-
-  get limit() {
-    return this._limit;
-  }
-
-  get status() {
-    return this._status;
-  }
-
-  get assignee() {
-    return this._assignee;
-  }
-
-  get keyword() {
-    return this._keyword;
-  }
-
-  get order() {
-    return this._order;
-  }
-
-  get orderBy() {
-    return this._orderBy;
-  }
-}
-
-
-
-export class ViewTaskEntity {
-  private readonly _userId: number;
-  private readonly _taskId: number
-
-  constructor(
-    userId: number,
-    taskId: number
-  ) {
-    this._userId = userId;
-    this._taskId = taskId;
-  }
-
-  get userId() {
-    return this._userId;
-  }
-
-  get taskId() {
-    return this._taskId;
   }
 }
