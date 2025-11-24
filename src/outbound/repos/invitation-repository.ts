322fecb9428a375433c09fd068955bfbd01b1245
HS-hand <shs0714@khu.ac.kr>
2025-11-24 14:49:56 +0900

    [Feat] invitation-repository.ts

--- src/outbound/repos/invitation-repository.ts
+++ src/outbound/repos/invitation-repository.ts
@@ -1,6 +1,7 @@
 import { PrismaClient } from "@prisma/client";
 import { BaseRepository } from "./base-repository";
 import {
+  InvitationEntity,
   NewInvitationEntity,
   PersistInvitationEntity,
 } from "../../domain/entites/member/invitation-entity";
@@ -14,10 +15,27 @@ export class InvitationRepository extends BaseRepository {
   async save(
     invitation: NewInvitationEntity
   ): Promise<PersistInvitationEntity> {
-    const data = invitation.toData();
-    const prismaInvitation = this._prismaClient.invitation.create({
-      data,
+    const createData = InvitationMapper.toCreateData(invitation);
+
+    const prismaInvitation = await this._prismaClient.invitation.create({
+      data: createData
     });
-    return InvitationMapper.toPersistEntity({...prismaInvitation})
+    return InvitationMapper.toPersistEntity(prismaInvitation);
+  }
+
+  async findByToken(token: string): Promise<InvitationEntity | null>{
+    const prismaInvitation = await this._prismaClient.invitation.findUnique({
+      where:{token}
+    });
+    if(!prismaInvitation){
+      return null;
+    }
+    return InvitationMapper.toPersistEntity(prismaInvitation);
+  }
+
+  async delete(token:string): Promise<void>{
+    await this._prismaClient.invitation.delete({
+      where:{token},
+    });
   }
 }
