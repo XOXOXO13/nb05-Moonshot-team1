b9461a6c6e76f8a4253143850e07caf02e38ba06
HS-hand <shs0714@khu.ac.kr>
2025-11-26 13:26:44 +0900

    fix

--- src/inbound/ports/I-services.ts
+++ src/inbound/ports/I-services.ts
@@ -1,9 +1,9 @@
 import { ITaskService } from "./services/I-task-service";
-import { IProjectService } from "./services/I-project-service";
 import { IUserService } from "./services/I-user-service";
 import { IAuthService } from "./services/I-auth-service";
 import { IInvitationService } from "./services/I-invitation-service";
 import { IMemberService } from "./services/I-member-service";
+import { IProjectService } from "./services/I-project-service";
 
 export interface IServices {
   task: ITaskService;
